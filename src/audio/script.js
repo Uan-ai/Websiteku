74f010debd98f3e9aad3910ba38ae75665923061
burhans_1140 <78636472+Uan-ai@users.noreply.github.com>
2023-07-09 00:13:54 +0700

    Add files via upload

---
+++ src/audio/script.js
@@ -0,0 +1,7 @@
+const sideDataMaster = document.queryselector(".sideitem1");
+const SideDataMaster1 = document.queryselector(".sideitem2");
+
+sideDataMaster.addEventListener("click", function() { SideDataMaster1.classlist.toggle("menu-active");
+  
+})
+
